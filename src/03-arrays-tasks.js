cb384d88cadbb4c4d6cf92e9bd10b43a04f9bcf0
Maria Golomb <marialukashuk.ml@gmail.com>
2019-12-22 01:55:35 +0300

    z

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -447,21 +447,26 @@ function toStringList(arr) {
  *      { country: 'Russia',  city: 'Saint Petersburg' }
  *    ]
  */
-function sortCitiesArray(/* arr */) {
-  /*
+function sortCitiesArray(arr) {
   return arr.sort((a, b) => {
     if (a.country < b.country) {
       return -1;
     }
-    if (a.country === b.country) {
-      if (a.city < b.city) {
-        return -1;
-      }
+    if (a.country > b.country) {
+      return 1;
+    }
+
+    if (a.city < b.city) {
+      return -1;
+    }
+    if (a.city > b.city) {
+      return 1;
     }
     return 0;
   });
-  */
-  throw new Error('Not implemented');
+
+
+  // throw new Error('Not implemented');
 }
 
 /**

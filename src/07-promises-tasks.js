940ea77db6ed026c5b5b80ecdfe10089aa09d8e3
Maria Golomb <marialukashuk.ml@gmail.com>
2019-12-22 03:03:08 +0300

    z

--- src/07-promises-tasks.js
+++ src/07-promises-tasks.js
@@ -113,9 +113,19 @@ function getFastestPromise(array) {
  *
  */
 function chainPromises(array, action) {
+  /*
   return new Promise((resolve) => {
     const arrOfRes = [];
     array.forEach((pr) => { pr.then((res) => arrOfRes.push(res)); });
+    resolve(arrOfRes);
+  }).then((res) => res.reduce(action));
+  */
+
+  return new Promise((resolve) => {
+    const arrOfRes = [];
+    for (let i = 0; i < array.length; i += 1) {
+      array[i].then((a) => arrOfRes.push(a));
+    }
     resolve(arrOfRes);
   }).then((res) => res.reduce(action));
 }
